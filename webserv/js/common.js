e2886fa8b77eee31a5d8afb7666a5cdf5319f878
= <=>
2017-09-14 13:42:56 +0300

    webserv

---
+++ webserv/js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		autoWidth:true,
+		items : 1
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	
+
+});
