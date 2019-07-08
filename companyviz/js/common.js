da931db41c51f02fa76aa0694046e28dee253581
= <=>
2019-07-08 11:32:54 +0300

    asd

--- companyviz/js/common.js
+++ companyviz/js/common.js
@@ -61,7 +61,14 @@ $(document).ready(function() {
     removalDelay: 160,
     preloader: false,
 
-    fixedContentPos: false
+    fixedContentPos: false,
+
+    gallery: {
+      tCounter: '%curr% из %total%',
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
 
   });
 });
