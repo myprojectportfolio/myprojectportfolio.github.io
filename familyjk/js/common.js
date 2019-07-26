762910c78ce763895a55cc010add07ed2c04b236
= <=>
2019-07-26 11:28:15 +0300

    qwertuy

--- familyjk/js/common.js
+++ familyjk/js/common.js
@@ -46,6 +46,38 @@ if ($(this).scrollTop() > 1){
 });
 
 $(document).ready(function() {
+  $('.popup-vimeo, .popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    gallery: {
+      tCounter: '%curr% из %total%',
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 400, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+  });
+});
+
+$(document).ready(function() {
   $('.zoom-gallery').magnificPopup({
     delegate: 'a',
     type: 'image',
